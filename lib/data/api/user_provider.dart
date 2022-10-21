d6381fd900575cc1d6776f119f85df0e3a27029a
Oscar Hernandez <oscar.hernandez@undostres.com.mx>
2022-10-21 17:03:50 -0400

    changes

--- lib/data/api/user_provider.dart
+++ lib/data/api/user_provider.dart
@@ -6,11 +6,7 @@ class UserProvider implements IUserProvider {
   @override
   Future<http.Response> verifyUserID(int userId) async {
     return http.post(
-      Uri.parse('https://test.undostres.com.mx/api/v1/app/isIdValid'),
-      // headers: {
-      //   "Content-type": "application/json",
-      //   "Accept": "*/*",
-      // },
+      Uri.parse('http://test.undostres.com.mx/api/v1/app/isIdValid'),
       body: jsonEncode(<String, int>{'id': userId, 'flag': 1}),
     );
   }
