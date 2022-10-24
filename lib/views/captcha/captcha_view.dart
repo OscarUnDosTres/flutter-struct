0b2cc3f8974ae0381f321e9f71483aa1b663a7ed
Oscar Hernandez <oscar.hernandez@undostres.com.mx>
2022-10-24 13:53:25 -0400

    captcha

--- lib/views/captcha/captcha_view.dart
+++ lib/views/captcha/captcha_view.dart
@@ -4,12 +4,12 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-// import 'dart:ui' as ui;
-// import 'dart:html' as html;
+import 'dart:ui' as ui;
+import 'dart:html' as html;
 
-// import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/services.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
 
 class CaptchaView extends StatefulWidget {
   const CaptchaView({super.key});
@@ -19,74 +19,74 @@ class CaptchaView extends StatefulWidget {
 }
 
 class _CaptchaView extends State<CaptchaView> {
-  late WebViewController webViewController;
+  // late WebViewController webViewController;
 
   @override
   void initState() {
     super.initState();
 
     // ignore: undefined_prefixed_name
-    // ui.platformViewRegistry.registerViewFactory(
-    //   "recaptcha",
-    //   (int viewId) => html.IFrameElement()
-    //     ..style.height = '100%'
-    //     ..style.width = '100%'
-    //     ..src = '/assets/html/recaptcha.html'
-    //     ..style.border = 'none',
-    // );
-    // html.window.onMessage.listen((event) {
-    //   // _bloc.captchaCompleted();
-    // });
+    ui.platformViewRegistry.registerViewFactory(
+      "recaptcha",
+      (int viewId) => html.IFrameElement()
+        ..style.height = '100%'
+        ..style.width = '100%'
+        ..src = 'https://recaptcha-fa095.web.app/'
+        ..style.border = 'none',
+    );
+    html.window.onMessage.listen((event) {
+      // _bloc.captchaCompleted();
+    });
 
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+    // if (Platform.isAndroid) WebView.platform = AndroidWebView();
   }
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //     appBar: AppBar(
-    //       title: const Image(
-    //         image: AssetImage('assets/images/undostres_logo.png'),
-    //         width: 180,
-    //       ),
-    //     ),
-    //     body: Container(
-    //       width: 400,
-    //       height: 200,
-    //       child: const Directionality(
-    //         textDirection: TextDirection.ltr,
-    //         child: HtmlElementView(
-    //           viewType: 'recaptcha',
-    //         ),
-    //       ),
-    //     ));
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Image(
-          image: AssetImage('assets/images/undostres_logo.png'),
-          width: 180,
+        appBar: AppBar(
+          title: const Image(
+            image: AssetImage('assets/images/undostres_logo.png'),
+            width: 180,
+          ),
         ),
-      ),
-      body: Center(
-          child: WebView(
-        initialUrl: "https://recaptcha-fa095.web.app/",
-        javascriptMode: JavascriptMode.unrestricted,
-        javascriptChannels: {
-          JavascriptChannel(
-              name: 'Captcha',
-              onMessageReceived: (JavascriptMessage message) {
-                //This is where you receive message from
-                //javascript code and handle in Flutter/Dart
-                //like here, the message is just being printed
-                //in Run/LogCat window of android studio
-                //print(message.message);
-              })
-        },
-        onWebViewCreated: (WebViewController w) {
-          webViewController = w;
-        },
-      )),
-    );
+        body: Container(
+          width: 400,
+          height: 200,
+          child: const Directionality(
+            textDirection: TextDirection.ltr,
+            child: HtmlElementView(
+              viewType: 'recaptcha',
+            ),
+          ),
+        ));
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Image(
+    //       image: AssetImage('assets/images/undostres_logo.png'),
+    //       width: 180,
+    //     ),
+    //   ),
+    //   body: Center(
+    //       child: WebView(
+    //     initialUrl: "https://recaptcha-fa095.web.app/",
+    //     javascriptMode: JavascriptMode.unrestricted,
+    //     javascriptChannels: {
+    //       JavascriptChannel(
+    //           name: 'Captcha',
+    //           onMessageReceived: (JavascriptMessage message) {
+    //             //This is where you receive message from
+    //             //javascript code and handle in Flutter/Dart
+    //             //like here, the message is just being printed
+    //             //in Run/LogCat window of android studio
+    //             //print(message.message);
+    //           })
+    //     },
+    //     onWebViewCreated: (WebViewController w) {
+    //       webViewController = w;
+    //     },
+    //   )),
+    // );
   }
 }
